2055e99b30caa6af54284ae6bbe014fae25f6634
Srinika-05 <srinika.krish@gmail.com>
2025-10-02 14:43:06 -0700

    Update 1player.js

--- 1player.js
+++ 1player.js
@@ -1,1 +1,32 @@
+function playerGuess(){
+    if (guessCount>=4) {
+        alert("Max guess reached - you lost");
+        secretNumber = 0;
+
+    }
+
+    var inputSecretNum = parseInt(secretNumber);
+    const userInput = parseInt(document.getElementById("userInput").value);
+    const label = document.getElementById("label");
+    guessCount = guessCount+1;
+ 
+    if (isNaN(userInput) || userInput == '')
+    {
+        alert("Input should be numerical")
+    }
+    if (inputSecretNum === userInput){
+        alert("You won!");
+        label.innerHTML = "";
+    }
+    if(inputSecretNum < userInput){
+        label.innerHTML = "Incorrect Guess, Try Lower";
+    }else{
+         label.innerHTML = "Incorrect Guess, Try Higher";
+    }
+
+    document.getElementById("userInput").value = "";
+    
+    document.getElementById("quantity").value = maxCount--;
+    
+}
 
